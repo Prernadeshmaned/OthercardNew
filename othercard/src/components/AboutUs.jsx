25cd339f11e7005b82a3f59981ce68309c52a333
Prernadeshmaned <prernadeshmane3467@gmail.com>
2025-09-30 17:39:18 +0530

    prerna change 5

--- othercard/src/components/AboutUs.jsx
+++ othercard/src/components/AboutUs.jsx
@@ -13,7 +13,7 @@ const AboutUs = () => {
                 <h1>Welcome to Mehandi AboutUs</h1>
                 <p>Manage your Mehandi services and bookings from here.</p>
 
-                <p>prerna change 4</p>
+                <p>prerna change 5</p>
 
                 {/* Back to Dashboard Button */}
                 <Button
